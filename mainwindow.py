beda410cb38b4bd3ccde06d6ee8c0d35c4f012d1
ehandywhyy <andyccs07@gmail.com>
2021-04-01 22:34:48 +0800

    Added base GUI

--- mainwindow.py
+++ mainwindow.py
@@ -6,7 +6,7 @@ from worker import Worker
 from keydialog import KeyDialog
 import time
 
-class UiMainWindow(QtWidgets.QStackedWidget):
+class UiMainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         # call the inherited classes __init__ method
@@ -17,8 +17,13 @@ class UiMainWindow(QtWidgets.QStackedWidget):
         uic.loadUi(f"{ABSOLUTE_PATH}/mainwindow.ui", self)
 
         # set gui window size
-        self.setMinimumSize(QtCore.QSize(500, 820))
-        self.setMaximumSize(QtCore.QSize(500, 820))
+        self.setMinimumSize(QtCore.QSize(500, 800))
+        self.setMaximumSize(QtCore.QSize(500, 800))
+        
+        self.startMainPg(self.sw_page)
+
+
+
 
         # keydialog = KeyDialog()
 
@@ -56,6 +61,59 @@ class UiMainWindow(QtWidgets.QStackedWidget):
     # def recurring_timer(self):
     #     self.printLogs(LOADER, ".")
 
+    def setInitNick(self):
+        if not self.init_input_nick.text():
+            pass
+        else: 
+            self.input_nickname.setText(self.init_input_nick.text())
+            self.current_nickname.setText(self.init_input_nick.text())
+            self.stopMainPg(self.sw_page)
+
+    def setNickname(self):
+        if not self.input_nickname.text():
+            pass
+        else: 
+            self.current_nickname.setText(self.input_nickname.text())
+
+    def startMainPg(self, sw_page):
+        self.init_set_nick_button.clicked.connect(lambda: self.setInitNick())
+        self.changePage(sw_page, 0)
+
+    def stopMainPg(self, sw_page):
+        self.init_set_nick_button.clicked.disconnect()
+        self.startContactPg(self.sw_page)
+
+    def startContactPg(self, sw_page):
+        self.set_nickname_button.clicked.connect(lambda: self.setNickname())
+        self.start_call_button.clicked.connect(lambda: self.startCallPg(sw_page))
+        # self.refresh_button.clicked.connect(lambda: self.())
+        self.changePage(sw_page, 1)
+
+    def stopContactPg(self):
+        self.set_nickname_button.clicked.disconnect()
+        self.start_call_button.clicked.disconnect()
+        self.refresh_button.clicked.disconnect()
+
+    def startCallPg(self, sw_page):
+        self.accept_call_button.clicked.connect(lambda: self.startChatPg(sw_page))
+        self.reject_call_button.clicked.connect(lambda: self.stopCallPg(sw_page))
+        self.changePage(sw_page, 2)
+
+    def stopCallPg(self, sw_page):
+        self.accept_call_button.clicked.disconnect()
+        self.reject_call_button.clicked.disconnect()
+        self.startContactPg(sw_page)
+
+    def startChatPg(self, sw_page):
+        self.end_call_button.clicked.connect(lambda: self.stopChatPg(sw_page))
+        # self.send_msg_button.clicked.connect(lambda: self.stopChatPg(sw_page))
+        self.changePage(sw_page, 3)
+
+    def stopChatPg(self, sw_page):
+        self.end_call_button.clicked.disconnect()
+        # self.send_msg_button.clicked.disconnect()
+        self.startContactPg(sw_page)
+
     def isSignalConnected(self, obj, name):
         index = obj.metaObject().indexOfMethod(name)
         if index > -1:
@@ -63,3 +121,6 @@ class UiMainWindow(QtWidgets.QStackedWidget):
             if method:
                 return obj.isSignalConnected(method)
         return False
+
+    def changePage(self, sw_page, i):
+        self.sw_page.setCurrentIndex(i)
