6bcba18e1c0a8dbbd22d64c09a63d6d595b9b949
milosaur <dylanykl@gmail.com>
2021-04-04 23:21:12 +0800

    bit encoding works for short messages

--- call_client.py
+++ call_client.py
@@ -1,38 +1,131 @@
 ##### Imports #####
 import pyaudio
 import socket
-import sys
+import threading
 
-##### Pyaudio Initialization #####
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-BUFFER=1024
+# Global variables
+message = ""
+message_binary = ""
+message_counter = 0
 
-p = pyaudio.PyAudio()
-input_stream = p.open(format=FORMAT,
-                            input=True, rate=RATE, channels=CHANNELS,
-                            frames_per_buffer=BUFFER)
+encode_data = False
+first_round = True
 
-
-##### Socket Initialization #####
-host = '192.168.108.6'
-port = 10001
-size = 1024
-
-# Call #
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-
-###### EXAMPLE OF KEY #######
 key = [1, 5, 3, 4, 0, 4, 2, 2]
-count = 0
+key_counter = 0
 
-while True:
-    data = input_stream.read(BUFFER)
-    ######## MODIFY BITS HERE #########
+end_counter = 0
 
-    s.send(data)
+##### Get the bits to OR or AND with the data #####
+def get_bitwise_data(value, position):
+    if value==0:
+        bitwise_str = "11111111"
+        final_bitwise_str = bitwise_str[:2+position] + "0" + bitwise_str[3 + position:]
+    else:
+        bitwise_str = "00000000"
+        final_bitwise_str = bitwise_str[:2 + position] + "1" + bitwise_str[3 + position:]
+    return int(final_bitwise_str, 2)
+
+def reset_global_variables():
+    global message, message_binary, message_counter, key_counter, encode_data, first_round, end_counter
+    message = ""
+    message_binary = ""
+    message_counter = 0
+
+    encode_data = False
+    first_round = True
+
+    key_counter = 0
+    end_counter = 0
+
+##### Call function for sending (pick up mic data and send) ######
+def sender(host, port):
+    global message, message_binary, message_counter, key_counter, encode_data, first_round, end_counter
+    ##### Pyaudio Initialization #####
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    BUFFER=1024
+
+    p = pyaudio.PyAudio()
+    input_stream = p.open(format=FORMAT,
+                                input=True, rate=RATE, channels=CHANNELS,
+                                frames_per_buffer=BUFFER)
+
+    ##### Socket Initialization #####
+    # Call #
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+
+    while True:
+        # Get data from mic
+        data = input_stream.read(BUFFER)
+
+        # If there is data to be encoded then encode the data before sending
+        if encode_data:
+
+            # Insert data
+            modified_data = b''
+            current_index = 0
+
+            if first_round:
+                # Set the start pattern (8 x 00001111)
+                for i in range(8):
+                    modified_data += bytes({int('00001111', 2)})
+                    current_index += 1
+                first_round = False
+
+            # Insert data until end of message binary or end of packet
+            while current_index < 4096 and message_counter < len(message_binary):
+                if message_binary[message_counter] == "1":
+                    modified_data += bytes({data[current_index] | get_bitwise_data(1, key[key_counter])})
+                else:
+                    modified_data += bytes({data[current_index] & get_bitwise_data(0, key[key_counter])})
+                key_counter = (key_counter + 1) % len(key)
+                message_counter += 1
+                current_index += 1
+
+            # If completed the message then start inserting the end pattern
+            if message_counter == len(message_binary):
+
+                # insert 8 OR until end of packet
+                while current_index < 4096 and end_counter < 8:
+                    modified_data += bytes({int('00001111', 2)})
+                    current_index += 1
+                    end_counter += 1
+
+            # If the end pattern is completed then done
+            if end_counter == 8:
+                while current_index < 4096:
+                    modified_data += bytes({data[current_index]})
+                    current_index += 1
+                reset_global_variables()
+
+            s.send(modified_data)
+            continue
+
+        # Send normal data when no message
+        s.send(data)
 
 
+if __name__ == '__main__':
+    host = '192.168.0.111'
+    port = 10001
+
+    sender_thread = threading.Thread(target=sender, args=(host, port,))
+    sender_thread.start()
+
+    while True:
+        print("Please enter a message: ")
+        message = str(input())
+        message_binary = bin(int.from_bytes(bytes(message, 'utf-8'), byteorder='little'))[2:]
+
+
+        print(int(len(message_binary) / 8))
+
+        if len(message_binary) % 8 != 0:
+            for i in range(8-(len(message_binary) % 8)):
+                message_binary = "0"+message_binary
+        print(message_binary)
+        encode_data = True
 
--- call_server.py
+++ call_server.py
@@ -1,32 +1,120 @@
 ##### Imports #####
 import pyaudio
 import socket
+import sys
+import threading
 
-##### Socket Initialization #####
-host = '192.168.108.6'
-port = 10001
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((host, port))
-sock.listen()
+message_gathering = False
 
-client, address = sock.accept()
+key = [1, 5, 3, 4, 0, 4, 2, 2]
+key_counter = 0
+blank_counter = 0
+end_counter = 0
+message_bytes = b''
+
+collecting_data = False
+finished_message = False
 
 
-##### Pyaudio Initialization #####
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-BUFFER=1024
-p = pyaudio.PyAudio()
-output_stream = p.open(format=pyaudio.paInt16,
-                              output=True, rate=44100, channels=2,
-                              frames_per_buffer=BUFFER)
+def reset_global_variables():
+    global key_counter, collecting_data, blank_counter, end_counter, message_bytes, finished_message
 
-print("Server is now running\n=======================")
+    key_counter = 0
+    collecting_data = False
+    blank_counter = 0
+    end_counter = 0
+    message_bytes = b''
+    finished_message = False
 
-while True:
-    data = client.recv(BUFFER)
-    if data:
-        print(data)
-        output_stream.write(data)
 
+def receiver(host, port):
+    global key_counter, collecting_data, blank_counter, end_counter, message_bytes, finished_message
+
+    ##### Socket Initialization #####
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen()
+
+    client, address = sock.accept()
+
+
+    ##### Pyaudio Initialization #####
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    BUFFER=1024
+    p = pyaudio.PyAudio()
+    output_stream = p.open(format=FORMAT,
+                                  output=True, rate=RATE, channels=CHANNELS,
+                                  frames_per_buffer=BUFFER)
+
+    print("Server is now running\n=======================")
+
+    while True:
+        data = client.recv(BUFFER)
+        if data:
+            output_stream.write(data)
+            current_index = 0
+
+            # Not currently collecting data then...
+            if not collecting_data:
+
+                # Search for 00001111 to signify start of message
+                for i in range(8):
+                    if data[i] == int('00001111', 2):
+                        blank_counter += 1
+                        current_index += 1
+                    else:
+                        blank_counter = 0
+                        current_index += 1
+                if blank_counter == 8:
+                    collecting_data = True
+
+            # if collecting data
+            if collecting_data:
+                end_counter = 0
+                print("here")
+                # keep collecting data until finding 8 x 00001111 or until end of packet
+                while end_counter < 8 and current_index < 1024:
+                    if data[current_index] == int('00001111', 2):
+                        end_counter += 1
+                    else:
+                        message_bytes += bytes({data[current_index]})
+                        end_counter = 0
+
+                    current_index += 1
+
+                # if end pattern is found then stop collecting data
+                if end_counter == 8:
+                    finished_message = True
+
+            if finished_message:
+                # Convert bytes to string binary
+                message_bin = ""
+                for byte in message_bytes:
+                    bin_str = bin(byte)[2:]
+                    for i in range(8 - len(bin_str)):
+                        bin_str = "0" + bin_str
+
+                    message_bin += bin_str[key[key_counter] + 2]
+                    key_counter = (key_counter + 1) % len(key)
+
+                # Convert string binary to string
+                message = ""
+                print(message_bin)
+                for i in range(0, len(message_bin), 8):
+                    print(bytes({int(message_bin[i:i + 8], 2)}).decode("utf-8"))
+                    message = bytes({int(message_bin[i:i + 8], 2)}).decode("utf-8") + message
+
+                # print
+                print(message)
+                reset_global_variables()
+
+
+
+
+if __name__ == '__main__':
+    host = '192.168.0.111'
+    port = 10001
+
+    receiver(host, port)
