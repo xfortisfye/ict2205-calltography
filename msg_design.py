e4b70b6de74ad6276ff8e886b1dddf875934b832
ehandywhyy <andyccs07@gmail.com>
2021-04-04 17:29:47 +0800

    Pre-update GUI for merge

---
+++ msg_design.py
@@ -0,0 +1,105 @@
+'''
+Credits from: https://forum.learnpyqt.com/t/cloud-around-the-text-in-qtextedit/318/2
+'''
+
+import sys
+
+from PyQt5.QtCore import QAbstractListModel, QMargins, QPoint, QRectF, QSize, Qt
+from PyQt5.QtGui import QColor, QPainter, QTextDocument, QTextOption
+
+# from PyQt5.QtGui import
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLineEdit,
+    QListView,
+    QMainWindow,
+    QPushButton,
+    QStyledItemDelegate,
+    QVBoxLayout,
+    QWidget,
+)
+
+USER_ME = 0
+USER_THEM = 1
+
+BUBBLE_COLORS = {USER_ME: "#90caf9", USER_THEM: "#a5d6a7"}
+
+BUBBLE_PADDING = QMargins(15, 5, 15, 5)
+TEXT_PADDING = QMargins(25, 15, 25, 15)
+
+class MessageDelegate(QStyledItemDelegate):
+    """
+    Draws each message.
+    """
+
+    def paint(self, painter, option, index):
+        # Retrieve the user,message uple from our model.data method.
+        user, text = index.model().data(index, Qt.DisplayRole)
+
+        # option.rect contains our item dimensions. We need to pad it a bit
+        # to give us space from the edge to draw our shape.
+
+        bubblerect = option.rect.marginsRemoved(BUBBLE_PADDING)
+        textrect = option.rect.marginsRemoved(TEXT_PADDING)
+
+        # draw the bubble, changing color + arrow position depending on who
+        # sent the message. the bubble is a rounded rect, with a triangle in
+        # the edge.
+        painter.setPen(Qt.NoPen)
+        color = QColor(BUBBLE_COLORS[user])
+        painter.setBrush(color)
+        painter.drawRoundedRect(bubblerect, 10, 10)
+
+        # draw the triangle bubble-pointer, starting from the top left/right.
+        if user == USER_ME:
+            p1 = bubblerect.topRight()
+        else:
+            p1 = bubblerect.topLeft()
+        painter.drawPolygon(p1 + QPoint(-20, 0), p1 + QPoint(20, 0), p1 + QPoint(0, 20))
+
+        toption = QTextOption()
+        toption.setWrapMode(QTextOption.WrapAtWordBoundaryOrAnywhere)
+
+        # draw the text
+        painter.setPen(Qt.black)
+        painter.drawText(QRectF(textrect), text, toption)
+
+    def sizeHint(self, option, index):
+        _, text = index.model().data(index, Qt.DisplayRole)
+        textrect = option.rect.marginsRemoved(TEXT_PADDING)
+
+        toption = QTextOption()
+        toption.setWrapMode(QTextOption.WrapAtWordBoundaryOrAnywhere)
+
+        doc = QTextDocument(text)
+        doc.setTextWidth(textrect.width())
+        doc.setDefaultTextOption(toption)
+        doc.setDocumentMargin(0)
+
+        textrect.setHeight(doc.size().height())
+        textrect = textrect.marginsAdded(TEXT_PADDING)
+        return textrect.size()
+
+
+class MessageModel(QAbstractListModel):
+    def __init__(self, *args, **kwargs):
+        super(MessageModel, self).__init__(*args, **kwargs)
+        self.messages = []
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # Here we pass the delegate the user, message tuple.
+            return self.messages[index.row()]
+
+    def rowCount(self, index):
+        return len(self.messages)
+
+    def add_message(self, who, text):
+        """
+        Add an message to our message list, getting the text from the QLineEdit
+        """
+        if text:  # Don't add empty strings.
+            # Access the list via the model.
+            self.messages.append((who, text))
+            # Trigger refresh.
+            self.layoutChanged.emit()
